de1903f13f867a8241bd1f0a5085ee1c779cf2f3
Okechukwu Bright Onwumere <brighthamza9@yahoo.com>
2025-02-16 21:35:24 +0100

    Initial commit

---
+++ src/main/java/com/amblessed/Student.java
@@ -0,0 +1,15 @@
+package com.amblessed;
+
+
+
+/*
+* @Project Name: junit5-crash-course
+* @Author: Okechukwu Bright Onwumere
+* @Created: 16-Feb-25
+*/
+
+
+
+
+public class Student {
+}
---
+++ src/test/java/com/amblessed/FactorialTest.java
@@ -0,0 +1,15 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+
+/*
+* @Project Name: junit5-crash-course
+* @Author: Okechukwu Bright Onwumere
+* @Created: 16-Feb-25
+*/
+
+
+
+
+class FactorialTest {
+  
+}
